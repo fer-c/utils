921257baad6320eba420239de7ef834d0ea54972
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2017-06-02 17:54:16 +0100

    updated licence

--- src/maps_utils.erl
+++ src/maps_utils.erl
@@ -1,3 +1,20 @@
+%% =============================================================================
+%% maps_utils - a collection of utilities functions for erlang maps.
+%%
+%% Copyright (c) 2016-1027 Ngineo Limited t/a Leapsight. All rights reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%    http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%% =============================================================================
 -module(maps_utils).
 
 
@@ -5,9 +22,6 @@
 %% =============================================================================
 %% VALIDATION 
 %% =============================================================================
-
-
-
 -define(ALLOW_NULL, true).
 -define(ALLOW_UNDEFINED, false).
 
@@ -154,9 +168,13 @@
 -export([without_paths/2]).
 
 
+
+
 %% =============================================================================
 %% API
 %% =============================================================================
+
+
 
 %% -----------------------------------------------------------------------------
 %% @doc
@@ -256,8 +274,6 @@ without_paths(Ps, M) ->
     erlang:error(error_type(M), [Ps, M]).
 
 
-
-
 %% -----------------------------------------------------------------------------
 %% @doc
 %% This function appends a new Value to the current list of values associated
@@ -278,6 +294,7 @@ append_path([], _, _) ->
 
 append_path(Path, Value, Map) when is_list(Path) ->
     do_append_list_path(Path, [Value], Map).
+
 
 %% -----------------------------------------------------------------------------
 %% @doc
