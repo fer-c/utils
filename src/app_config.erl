c43c846a4e0fd044d21a1df351d6e17fa2c49995
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2020-01-27 14:07:50 +0000

    added event callbacks to app_config

--- src/app_config.erl
+++ src/app_config.erl
@@ -24,13 +24,46 @@
 -module(app_config).
 
 -define(ERROR, '$error_badarg').
+-define(OPTS_KEY, '$app_config_opts').
 
 -export([get/2]).
 -export([get/3]).
 -export([init/1]).
+-export([init/2]).
 -export([set/3]).
 
 -compile({no_auto_import, [get/1]}).
+
+
+
+
+%% =============================================================================
+%% CALLBACKS
+%% =============================================================================
+
+
+
+-callback will_set(Key :: atom(), Value :: any()) ->
+    {ok, NewValue :: any()} | {error, Reason :: any()}.
+
+
+-callback will_set(
+    Key :: atom(), Value :: any(), Path :: list(), PathValue :: any()) ->
+    {ok, NewValue :: any()} | {error, Reason :: any()}.
+
+
+-callback on_set(Key :: atom(), Value :: any()) -> ok.
+
+
+-callback on_set(
+    Key :: atom(), Value :: any(), Path :: list(), PathValue :: any()) ->
+    ok.
+
+
+-optional_callbacks([on_set/2]).
+-optional_callbacks([on_set/4]).
+-optional_callbacks([will_set/2]).
+-optional_callbacks([will_set/4]).
 
 
 
@@ -39,7 +72,10 @@
 %% =============================================================================
 
 
-
+%% -----------------------------------------------------------------------------
+%% @doc
+%% @end
+%% -----------------------------------------------------------------------------
 init(App) ->
     %% Init from environment
     Config0 = application:get_all_env(App),
@@ -48,6 +84,21 @@ init(App) ->
     %% We set configs at first level only
     _ = [set(App, Key, Value) || {Key, Value} <- Config1],
     ok.
+
+
+%% -----------------------------------------------------------------------------
+%% @doc Initilises and app config with options.
+%% Options are:
+%%
+%% * callback_mod - the callback module implement some or all of the callbacks
+%% @end
+%% -----------------------------------------------------------------------------
+init(App, Opts) when is_map(Opts) andalso map_size(Opts) == 0 ->
+    init(App);
+
+init(App, #{callback_mod := _} = Opts) ->
+    ok = do_set(App, ?OPTS_KEY, Opts),
+    init(App).
 
 
 %% -----------------------------------------------------------------------------
@@ -92,11 +143,35 @@ get(App, Key, Default) ->
 %% @doc
 %% @end
 %% -----------------------------------------------------------------------------
--spec set(App :: atom(), Key :: atom() | tuple(), Default :: term()) -> ok.
+-spec set(
+    App :: atom(),
+    Key :: list() | atom() | tuple() | binary(),
+    Default :: term()) -> ok.
 
-set(App, Key, Value) when is_atom(App) andalso is_atom(Key) ->
-    application:set_env(App, Key, Value),
-    persistent_term:put({App, Key}, Value).
+set(App, Key, Value) when is_atom(App) ->
+    case will_set(App, Key, Value) of
+        {ok, NewValue} ->
+            ok = do_set(App, Key, NewValue),
+            on_set(App, Key, NewValue);
+        {error, Reason} ->
+            error(Reason)
+    end;
+
+set(App, [Key|Path], PathValue) when is_atom(App) ->
+    Value = case get(App, Key, undefined) of
+        Term when is_map(Term) orelse is_list(Term) ->
+            Term;
+        _ ->
+            error(badarg)
+    end,
+
+    case will_set(App, Key, Value, Path, PathValue) of
+        {ok, NewValue} ->
+            ok = do_set(App, Key, NewValue),
+            on_set(App, Key, Value, Path, PathValue);
+        {error, Reason} ->
+            error(Reason)
+    end.
 
 
 
@@ -150,3 +225,68 @@ maybe_badarg(?ERROR) ->
 
 maybe_badarg(Term) ->
     Term.
+
+
+%% @private
+do_set(App, Key, Value) ->
+    application:set_env(App, Key, Value),
+    persistent_term:put({App, Key}, Value).
+
+%% @private
+will_set(App, Key, Value) ->
+    case get(App, ?OPTS_KEY, undefined) of
+        #{callback_mod := Mod} ->
+            case erlang:function_exported(Mod, will_set, 2) of
+                true ->
+                    Mod:will_set(Key, Value);
+                false ->
+                    {ok, Value}
+            end;
+        _ ->
+            {ok, Value}
+    end.
+
+
+%% @private
+on_set(App, Key, Value) ->
+    case get(App, ?OPTS_KEY, undefined) of
+        #{callback_mod := Mod} ->
+            case erlang:function_exported(Mod, on_set, 2) of
+                true ->
+                    Mod:on_set(Key, Value);
+                false ->
+                    ok
+            end;
+        _ ->
+            ok
+    end.
+
+
+%% @private
+will_set(App, Key, Value, Path, PathValue) ->
+    case get(App, ?OPTS_KEY, undefined) of
+        #{callback_mod := Mod} ->
+            case erlang:function_exported(Mod, will_set, 4) of
+                true ->
+                    Mod:will_set(Key, Value, Path, PathValue);
+                false ->
+                    {ok, Value}
+            end;
+        _ ->
+            {ok, Value}
+    end.
+
+
+%% @private
+on_set(App, Key, Value, Path, PathValue) ->
+    case get(App, ?OPTS_KEY, undefined) of
+        #{callback_mod := Mod} ->
+            case erlang:function_exported(Mod, on_set, 4) of
+                true ->
+                    Mod:on_set(Key, Value, Path, PathValue);
+                false ->
+                    ok
+            end;
+        _ ->
+            ok
+    end.
