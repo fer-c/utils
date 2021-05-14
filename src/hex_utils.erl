11de217eef0d442598286b45ec3836e01e8a1ed5
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2021-05-14 09:13:28 +0100

    added hexstr bin input
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- src/hex_utils.erl
+++ src/hex_utils.erl
@@ -1,13 +1,28 @@
--module(hex_utils).
--export([bin_to_hexstr/1,hexstr_to_bin/1]).
-
+%% -----------------------------------------------------------------------------
+%% @doc
+%%
 %% From Steve Vinoski (http://necrobious.blogspot.co.uk/2008/03/binary-to-hex-string-back-to-binary-in.html)
+%% @end
+%% -----------------------------------------------------------------------------
+-module(hex_utils).
+
+
+-export([bin_to_hexstr/1]).
+-export([hexstr_to_bin/1]).
+
+
+
+%% =============================================================================
+%% API
+%% =============================================================================
+
+
 
 %% -----------------------------------------------------------------------------
 %% @doc
 %% @end
 %% -----------------------------------------------------------------------------
-bin_to_hexstr(Bin) ->
+bin_to_hexstr(Bin) when is_binary(Bin) ->
   lists:flatten([io_lib:format("~2.16.0B", [X]) ||
     X <- binary_to_list(Bin)]).
 
@@ -16,7 +31,10 @@ bin_to_hexstr(Bin) ->
 %% @doc
 %% @end
 %% -----------------------------------------------------------------------------
-hexstr_to_bin(S) ->
+hexstr_to_bin(S) when is_binary(S) ->
+  hexstr_to_bin(binary_to_list(S));
+
+hexstr_to_bin(S) when is_list(S) ->
   hexstr_to_bin(S, []).
 
 
